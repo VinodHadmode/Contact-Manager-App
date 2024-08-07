c9beb4898c80cb99b561de57bcec565e88333ac6
VinodHadmode <vinodhadmode@gmail.com>
2024-08-07 13:14:18 +0530

    Refresh frontend directory tracking

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Contact Manager App using react & bootstrap</h2>
+    </div>
+  );
+}
+
+export default App;
